1fecffabe75bef3f24410f1a0588d2c90283f93c
Rocco Theodorakis <roccotheo10@gmail.com>
2026-09-09 02:41:36 +0000

    added json to listener callback of plc hmi listener

--- pa_warehouse_status/pa_warehouse_status/plc_hmi_listener.py
+++ pa_warehouse_status/pa_warehouse_status/plc_hmi_listener.py
@@ -1,4 +1,5 @@
 import rclpy
+import json
 from rclpy.node import Node
 from std_msgs.msg import String
 
@@ -8,7 +9,20 @@ class PLCHMIListener (Node):
         self.subscription = self.create_subscription(String, 'hmi/unified_status',self.listener_callback,10)
     
     def listener_callback(self,msg):
-        self.get_logger().info(f"PLC information: {msg.data}")
+        try:
+            data = json.loads(msg.data) # parse JSON string into Python dict
+            stamp = data["stamp"]
+            box = data["box"]
+            counts = data["counts"]
+            print("📥 Received PLC status:")
+            print(f" ⏱ Time: {stamp['sec']}.{stamp['nanosec']}")
+            print(f" 📦 Box weight raw={box['weight_raw']}")
+            print(f" 📍 Location: {box['location']}")
+            print(f" 🔢 Counts: big={counts['big']}, medium={counts['medium']}, "
+                f"small={counts['small']}, total={counts['total']}")
+            print() # ���� empty line at the end
+        except Exception as e:
+            self.get_logger().error(f"Failed to parse JSON: {e}\nRaw msg={msg.data}")
         
 
 
